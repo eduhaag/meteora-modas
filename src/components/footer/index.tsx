aaf08d3278b63dfec7a6bd300e278cbf98255ee5
Eduardo Haag <eduardof.haag@gmail.com>
2023-07-10 18:15:42 -0300

    update footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -4,7 +4,8 @@ export function Footer() {
   return (
     <FooterContainer>
       <p>
-        2023 © Desenvolvido por Alura | Projeto fictício sem fins comerciais.
+        2023 © Desenvolvido Eduardo Felipe Haag | Projeto fictício sem fins
+        comerciais.
       </p>
     </FooterContainer>
   )
